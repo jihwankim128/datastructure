1e1faacc47738407c6a38617d3a8fa9d28dd55a9
jihwan <jihwan1535@nate.com>
2024-02-07 11:44:48 +0900

    feat: 이중 연결리스트 노드 구현 #4

---
+++ #2/DoublyLinkedListNode.h
@@ -0,0 +1,49 @@
+#pragma once
+
+// Prototype
+template <typename T>
+class Node {
+public:
+    Node(T data);
+    
+    T getData() const;
+    Node* getNext() const;
+    Node* getPrev() const;
+
+    void setNext(Node* node);
+    void setPrev(Node* node);
+
+private:
+    T data;
+    Node* next;
+    Node* prev;
+};
+
+template <typename T>
+Node<T>::Node(T data) : data(data), next(nullptr), prev(nullptr) {}
+
+template <typename T>
+T Node<T>::getData() const {
+    return data;
+}
+
+template <typename T>
+Node<T>* Node<T>::getNext() const {
+    return next;
+}
+
+template <typename T>
+Node<T>* Node<T>::getPrev() const {
+    return prev;
+}
+
+template <typename T>
+void Node<T>::setNext(Node* node) {
+    next = node;
+}
+
+template <typename T>
+void Node<T>::setPrev(Node* node) {
+    prev = node;
+}
+
