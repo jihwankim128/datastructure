64d0b326b4e4c4ba8e463c8dbd063664801fe08a
Laboon <jihwan1535@nate.com>
2025-09-09 23:36:07 +0900

    Title: 기울어진 직사각형, Time: 129ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250909/기울어진 직사각형/slanted-rectangle.java
+++ 250909/기울어진 직사각형/slanted-rectangle.java
@@ -1,29 +1,36 @@
 import java.util.Scanner;
-public class Main {
 
+public class Main {
     static int[][] grid = new int[21][21];
     static int n;
     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < n; j++)
-                grid[i][j] = sc.nextInt();
-                
-        System.out.println(solve());
+        readGrid(sc);
+        System.out.println(findMaxTiltedRectPerimeter());
     }
-
-    static int solve() {
-        int maxSum = 0;
+    
+    static void readGrid(Scanner sc) {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                maxSum = Math.max(maxSum, findMaxFromCenter(i, j));
+                grid[i][j] = sc.nextInt();
+            }
+        }
+    }
+    
+    static int findMaxTiltedRectPerimeter() {
+        int maxSum = 0;
+        
+        // 모든 가능한 중심점에서 시도
+        for (int centerX = 0; centerX < n; centerX++) {
+            for (int centerY = 0; centerY < n; centerY++) {
+                maxSum = Math.max(maxSum, findMaxFromCenter(centerX, centerY));
             }
         }
         return maxSum;
     }
-
+    
     static int findMaxFromCenter(int centerX, int centerY) {
         int maxSum = 0;
         
@@ -31,46 +38,44 @@ public class Main {
         for (int k1 = 1; k1 < n; k1++) {        // 세로 방향 반지름
             for (int k2 = 1; k2 < n; k2++) {    // 가로 방향 반지름
                 int perimeterSum = calculatePerimeter(centerX, centerY, k1, k2);
-                maxSum = Math.max(maxSum, perimeterSum);
+                if (perimeterSum > 0) {  // 유효한 테두리가 있는 경우만
+                    maxSum = Math.max(maxSum, perimeterSum);
+                }
             }
         }
         return maxSum;
     }
-
-    static boolean canDraw(int x, int y, int height, int width) {
-        // 왼쪽 대각선 아래로 내려갔을 때 벗어나면 못함
-        int ex = x + height, ey = y - height;
-        if (ex >= n || ey < 0) return false;
-        // 왼쪽 끝점에서 오른쪽 대각선 아래로 내려갔을 때 벗어나면 못함
-        if (ex + width >= n || ey + width >= n) return false;
-        return true;
-    }
-
+    
     static int calculatePerimeter(int centerX, int centerY, int k1, int k2) {
         int sum = 0;
-
+        
         // 상단 대각선 (↗ 방향)
         sum += addDiagonalLine(centerX - k1, centerY, k2, 1, 1);
+        
         // 우측 대각선 (↘ 방향)  
         sum += addDiagonalLine(centerX, centerY + k2, k1, 1, -1);
+        
         // 하단 대각선 (↙ 방향)
         sum += addDiagonalLine(centerX + k1, centerY, k2, -1, -1);
+        
         // 좌측 대각선 (↖ 방향)
         sum += addDiagonalLine(centerX, centerY - k2, k1, -1, 1);
         
         return sum;
     }
-
+    
     static int addDiagonalLine(int startX, int startY, int length, int dirX, int dirY) {
         int sum = 0;
         
         for (int i = 0; i < length; i++) {
             int x = startX + i * dirX;
             int y = startY + i * dirY;
-            if (!isInBounds(x, y)) {
-                return 0;
+            
+            if (isInBounds(x, y)) {
+                sum += grid[x][y];
+            } else {
+                return 0;  // 경계를 벗어나면 유효하지 않음
             }
-            sum += grid[x][y];
         }
         return sum;
     }
