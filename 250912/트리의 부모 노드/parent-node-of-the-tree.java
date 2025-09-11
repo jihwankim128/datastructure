9c74f18a321f802d6046c1d9e6f2aaeb89d18edb
Laboon <jihwan1535@nate.com>
2025-09-12 01:38:11 +0900

    Title: 트리의 부모 노드, Time: 1918ms, Memory: 69MB, Status: Passed - Codetree

--- 250912/트리의 부모 노드/parent-node-of-the-tree.java
+++ 250912/트리의 부모 노드/parent-node-of-the-tree.java
@@ -1,26 +1,44 @@
-import java.util.Scanner;
+import java.util.*;
+
 public class Main {
 
     static int n, x, y;
+    static ArrayList<Integer>[] trees;
     static int[] roots;
+    static boolean[] visited;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
         roots = new int[n + 1];
+        trees = new ArrayList[n + 1];
+        visited = new boolean[n + 1];
+
+        for (int i = 0; i <= n; i++) {
+            trees[i] = new ArrayList<Integer>();
+        }
 
         for (int i = 1; i < n; i++) {
             x = sc.nextInt();
             y = sc.nextInt();
-            roots[y] = x;
+            trees[x].add(y);
+            trees[y].add(x);
         }
         
-        solve();
-    }
-
-    static void solve() {
+        solve(1);
         for (int i = 2; i <= n; i++) {
             System.out.println(roots[i]);
+        }
+    }
+
+    static void solve(int root) {
+        visited[root] = true;
+        for (int child: trees[root]) {
+            if (visited[child]) {
+                continue;
+            }
+            roots[child] = root;
+            solve(child);
         }
     }
 }
