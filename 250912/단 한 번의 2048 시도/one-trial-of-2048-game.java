1777d0483ae55e18d826cc060bd4aad4d2717dd7
Laboon <jihwan1535@nate.com>
2025-09-12 17:22:44 +0900

    Title: 단 한 번의 2048 시도, Time: 126ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250912/단 한 번의 2048 시도/one-trial-of-2048-game.java
+++ 250912/단 한 번의 2048 시도/one-trial-of-2048-game.java
@@ -3,6 +3,14 @@ import java.util.Scanner;
 public class Main {
 
     static int[][] grid = new int[4][4];
+    static int[] dir = new int[128]; // ascii로 방향 표현
+
+    static {
+        dir['L'] = 0;  // 왼쪽 (기본, 회전 없음)
+        dir['U'] = 1;  // 위쪽 (시계방향 90도)
+        dir['R'] = 2;  // 오른쪽 (시계방향 180도)
+        dir['D'] = 3;  // 아래쪽 (시계방향 270도)
+    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -11,85 +19,40 @@ public class Main {
                 grid[i][j] = sc.nextInt();
             }
         }
-        char dir = sc.next().charAt(0);
-        solve(dir);
+        char direction = sc.next().charAt(0);
+        solve(direction);
     }
 
-    static void solve(char dir) {
-        if (dir == 'R') {
-            moveRight();
-        } else if (dir == 'L') {
-            moveLeft();
-        } else if (dir == 'D') {
-            moveDown();
-        } else {
-            moveUp();
+    static void solve(char direction) {
+        int rotations = dir[direction];
+        
+        // 회전시켜서 왼쪽으로 떨어뜨리기
+        for (int i = 0; i < rotations; i++) {
+            rotateClockwise();
         }
+        
+        drop();
+        
+        // 원래 방향으로 되돌리기
+        for (int i = 0; i < (4 - rotations) % 4; i++) {
+            rotateClockwise();
+        }
+        
         print();
     }
 
-    static void moveUp() {
-        // 모든 숫자가 위쪽으로 이동한다.
-        for (int i = 0; i < 4; i++) {
-            // 아래쪽의 숫자 중 더할 수 있는 경우 더한다.
-            for (int j = 0; j <= 3; j++) {
-                // 현재 위치에서 맨 아래쪽에 있는 값을 찾는다.
-                int down = j;
-                while (++down < 4) {
-                    if (grid[i][down] != 0) break;
-                }
-                //현재 숫자와 같은 경우 더한다.
-                if (down < 4 && grid[j][i] == grid[down][i]) {
-                    grid[j][i] += grid[j][i];
-                    grid[down][i] = 0;
-                }
-                // 위쪽으로 이동한다. 
-                for (int up = j - 1; up >= 0; up--) {
-                    if (grid[up][i] > 0) break;
-                    grid[up][i] = grid[up + 1][i];
-                    grid[up + 1][i] = 0;
-                }
-            }
-        }
-    }
-
-    static void moveDown() {
-        // 모든 숫자가 아래쪽으로 이동한다.
-        for (int i = 0; i < 4; i++) {
-            // 위쪽의 숫자 중 더할 수 있는 경우 더한다.
-            for (int j = 3; j >= 0; j--) {
-                // 현재 위치에서 맨 위에 있는 값을 찾는다.
-                int up = j;
-                while (--up >= 0) {
-                    if (grid[up][i] != 0) break;
-                }
-                //현재 숫자와 같은 경우 더한다.
-                if (up >= 0 && grid[j][i] == grid[up][i]) {
-                    grid[j][i] += grid[j][i];
-                    grid[up][i] = 0;
-                }
-                // 아래쪽으로 이동한다. 
-                for (int down = j + 1; down < 4; down++) {
-                    if (grid[down][i] > 0) break;
-                    grid[down][i] = grid[down - 1][i];
-                    grid[down - 1][i] = 0;
-                }
-            }
-        }
-    }
-
-    static void moveLeft() {
+    static void drop() {
         // 모든 숫자가 왼쪽으로 이동한다.
         for (int i = 0; i < 4; i++) {
             // 오른쪽의 숫자 중 더할 수 있는 경우 더한다.
             for (int j = 0; j <= 3; j++) {
-                // 현재 위치에서 맨 왼쪽에 있는 값을 찾는다.
+                // 현재 위치에서 맨 오른쪽에 있는 값을 찾는다.
                 int right = j;
                 while (++right < 4) {
                     if (grid[i][right] != 0) break;
                 }
                 //현재 숫자와 같은 경우 더한다.
-                if (right < 4 && grid[i][j] == grid[i][right]) {
+                if (right < 4 && grid[i][j] != 0 && grid[i][j] == grid[i][right]) {
                     grid[i][j] += grid[i][j];
                     grid[i][right] = 0;
                 }
@@ -103,27 +66,16 @@ public class Main {
         }
     }
 
-    static void moveRight() {
-        // 모든 숫자가 오른쪽으로 이동한다.
+    static void rotateClockwise() {
+        int[][] temp = new int[4][4];
         for (int i = 0; i < 4; i++) {
-            // 왼쪽의 숫자 중 더할 수 있는 경우 더한다.
-            for (int j = 3; j >= 0; j--) {
-                // 현재 위치에서 맨 왼쪽에 있는 값을 찾는다.
-                int left = j;
-                while (--left >= 0) {
-                    if (grid[i][left] != 0) break;
-                }
-                //현재 숫자와 같은 경우 더한다.
-                if (left >= 0 && grid[i][j] == grid[i][left]) {
-                    grid[i][j] += grid[i][j];
-                    grid[i][left] = 0;
-                }
-                // 오른쪽으로 이동한다. 
-                for (int right = j + 1; right < 4; right++) {
-                    if (grid[i][right] > 0) break;
-                    grid[i][right] = grid[i][right - 1];
-                    grid[i][right - 1] = 0;
-                }
+            for (int j = 0; j < 4; j++) {
+                temp[j][3 - i] = grid[i][j];
+            }
+        }
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                grid[i][j] = temp[i][j];
             }
         }
     }
