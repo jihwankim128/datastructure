c00ed93839271a6ec6bc5b3a751cf3ae0de5df2f
Laboon <jihwan1535@nate.com>
2025-09-14 00:02:35 +0900

    Title: 벽 짚고 미로 탈출하기, Time: 149ms, Memory: 11MB, Status: Passed - Codetree

---
+++ 250914/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.java
@@ -0,0 +1,88 @@
+// PM 11:04
+import java.util.Scanner;
+
+public class Main {
+
+    static int n, x, y;
+    static char[][] maze = new char[101][101];
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        x = sc.nextInt() - 1;
+        y = sc.nextInt() - 1;
+        for (int i = 0; i < n; i++) {
+            String line = sc.next();
+            for (int j = 0; j < n; j++) {
+                maze[i][j] = line.charAt(j);
+            }
+        }
+        
+        solve();
+    }
+
+    static int[] dx = {-1, 0, 1, 0}, dy = {0, -1, 0, 1};
+    static boolean[][][] visited = new boolean[101][101][4];
+
+    static void debugging() {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(visited[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("-----------");
+    }
+    static void solve() {
+        // 시작 방향을 오른쪽으로 한칸 이동한다.
+        int dir = 3;
+        visited[x][y][dir] = true;
+        int count = 0;
+
+        if (isAllWall()) {
+            System.out.println(-1);
+            return;
+        }
+        
+        // 벗어날 때까지 반복한다.
+        while (true) { 
+            //debugging();
+            int nx = x + dx[dir];
+            int ny = y + dy[dir];
+            if (nx < 0 || nx >= n || ny < 0 || ny >= n) {
+                break;
+            }
+            // 이동하는 방향이 만약 벽이라면 왼쪽으로 90도 회전한다.
+            if (maze[nx][ny] == '#') {
+                dir = (dir + 1) % 4;
+                continue;
+            }
+
+            if (visited[nx][ny][dir]) {
+                System.out.println(-1);
+                return;
+            }
+            // 이동하는 방향이 벽이 아니라면 앞으로 이동한다.
+            visited[nx][ny][dir] = true;
+            x = nx;
+            y = ny;
+            count++;
+
+            // 앞으로 이동 중 아래에 벽이 없으면 오른쪽 회전 후 벽의 측면을 따라 이동한다.
+            int ndir = (dir + 3) % 4;
+            if (maze[nx][ny] == '.') dir = ndir;
+        }
+
+        System.out.println(count + 1);
+    }
+
+    static boolean isAllWall() {
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if (nx < 0 || nx >= n || ny < 0 || ny >= n) return false;
+            if (maze[nx][ny] != '#') return false;
+        }
+        return true;
+    }
+}
