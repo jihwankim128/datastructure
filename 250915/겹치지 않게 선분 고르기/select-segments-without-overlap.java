f4c6c4cabb3a9d47d626fad54c8a654e0adab4b5
Laboon <jihwan1535@nate.com>
2025-09-15 14:58:05 +0900

    Title: 겹치지 않게 선분 고르기, Time: 124ms, Memory: 10MB, Status: Passed - Codetree

--- 250915/겹치지 않게 선분 고르기/select-segments-without-overlap.java
+++ 250915/겹치지 않게 선분 고르기/select-segments-without-overlap.java
@@ -1,67 +1,75 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+
+class Pair { 
+    int x, y;
+    public Pair(int x, int y) { 
+        this.x = x; 
+        this.y = y; 
+    } 
+}
 
 public class Main {
-
-    static int n;
-    static int[][] segments;
-    static boolean[] coordinate = new boolean[1001];
-    static int maxCnt;
+    public static final int MAX_N = 15;
     
+    public static int n;
+    public static Pair[] segments = new Pair[MAX_N];
+    
+    public static int ans;
+    public static ArrayList<Pair> selectedSegs = new ArrayList<>();
+    
+    public static boolean overlapped(Pair seg1, Pair seg2) {
+        int ax1 = seg1.x;
+        int ax2 = seg1.y;
+
+        int bx1 = seg2.x;
+        int bx2 = seg2.y;
+    
+        // 두 선분이 겹치는지 여부는
+        // 한 점이 다른 선분에 포함되는 경우로 판단 가능합니다. 
+        return (ax1 <= bx1 && bx1 <= ax2) || (ax1 <= bx2 && bx2 <= ax2) ||
+               (bx1 <= ax1 && ax1 <= bx2) || (bx1 <= ax2 && ax2 <= bx2);
+    }
+    
+    public static boolean possible() {
+        // 단 한쌍이라도 선분끼리 겹치면 안됩니다.
+        for(int i = 0; i < (int)selectedSegs.size(); i++)
+            for(int j = i + 1; j < (int)selectedSegs.size(); j++)
+                if(overlapped(selectedSegs.get(i), selectedSegs.get(j)))
+                    return false;
+    
+        return true;
+    }
+    
+    public static void findMaxSegments(int cnt) {
+        if(cnt == n) {
+            if(possible())
+                ans = Math.max(ans, (int) selectedSegs.size());
+            return;
+        }
+        
+        selectedSegs.add(segments[cnt]);
+        findMaxSegments(cnt + 1);
+        selectedSegs.remove(selectedSegs.size() - 1);
+        
+        findMaxSegments(cnt + 1);
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
-        segments = new int[n][2];
-        
-        for (int i = 0; i < n; i++) {
-            segments[i][0] = sc.nextInt();
-            segments[i][1] = sc.nextInt();
-        }
-        
-        solve(0, 0);
-        System.out.println(maxCnt);
-    }
 
-    static void solve(int idx, int currentCnt) {
-        // 가지치기: 남은 선분을 모두 선택해도 현재 최댓값을 넘을 수 없다면 종료
-        if (currentCnt + (n - idx) <= maxCnt) return;
+        for(int i = 0; i < n; i++)
+            segments[i] = new Pair(0, 0);
         
-        if (idx == n) {
-            maxCnt = Math.max(currentCnt, maxCnt);
-            return;
+        for(int i = 0; i < n; i++) {
+            int x1 = sc.nextInt();
+            int x2 = sc.nextInt();
+            segments[i] = new Pair(x1, x2);
         }
 
-        int p1 = segments[idx][0];
-        int p2 = segments[idx][1];
+        findMaxSegments(0);
         
-        // 현재 선분을 선택하는 경우
-        if (canSelect(p1, p2)) {
-            select(p1, p2);
-            solve(idx + 1, currentCnt + 1);
-            unselect(p1, p2);
-        }
-        
-        // 현재 선분을 선택하지 않는 경우
-        solve(idx + 1, currentCnt);
-    }
-
-    static boolean canSelect(int p1, int p2) {
-        for (int pos = p1; pos <= p2; pos++) {
-            if (coordinate[pos]) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    static void select(int p1, int p2) {
-        for (int pos = p1; pos <= p2; pos++) {
-            coordinate[pos] = true;
-        }
-    }
-    
-    static void unselect(int p1, int p2) {
-        for (int pos = p1; pos <= p2; pos++) {
-            coordinate[pos] = false;
-        }
+        System.out.print(ans);
     }
 }
