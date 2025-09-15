cfb59a7e9f160c61d23a08c67ac69e2f72ae16cd
Laboon <jihwan1535@nate.com>
2025-09-15 14:57:17 +0900

    Title: 겹치지 않게 선분 고르기, Time: 124ms, Memory: 10MB, Status: Passed - Codetree

--- 250915/겹치지 않게 선분 고르기/select-segments-without-overlap.java
+++ 250915/겹치지 않게 선분 고르기/select-segments-without-overlap.java
@@ -1,49 +1,67 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
 
-    static class Pair {
-        int x, y;
-        Pair (int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
-
     static int n;
-    static ArrayList<Pair> coordinate = new ArrayList<>();
-
+    static int[][] segments;
+    static boolean[] coordinate = new boolean[1001];
+    static int maxCnt;
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
+        segments = new int[n][2];
         
         for (int i = 0; i < n; i++) {
-            int x = sc.nextInt();
-            int y = sc.nextInt();
-            coordinate.add(new Pair(x, y));
+            segments[i][0] = sc.nextInt();
+            segments[i][1] = sc.nextInt();
         }
         
-        solve();
+        solve(0, 0);
+        System.out.println(maxCnt);
     }
 
-
-    static void solve() {
-        // 끝점 기준으로 정렬 (끝점이 같으면 시작점 기준)
-        coordinate.sort((p1, p2) -> {
-            if (p1.y == p2.y) return p1.x - p2.x;
-            return p1.y - p2.y;
-        });
+    static void solve(int idx, int currentCnt) {
+        // 가지치기: 남은 선분을 모두 선택해도 현재 최댓값을 넘을 수 없다면 종료
+        if (currentCnt + (n - idx) <= maxCnt) return;
         
-        Pair prev = coordinate.get(0);
-        int cnt = 1;
-        for (int i = 1; i < n; i++) {
-            Pair now = coordinate.get(i);
-            // 현재 선분의 시작점이 이전 선분의 끝점보다 뒤에 있어야 함
-            if (now.x > prev.y) {
-                prev = now;
-                cnt++;
+        if (idx == n) {
+            maxCnt = Math.max(currentCnt, maxCnt);
+            return;
+        }
+
+        int p1 = segments[idx][0];
+        int p2 = segments[idx][1];
+        
+        // 현재 선분을 선택하는 경우
+        if (canSelect(p1, p2)) {
+            select(p1, p2);
+            solve(idx + 1, currentCnt + 1);
+            unselect(p1, p2);
+        }
+        
+        // 현재 선분을 선택하지 않는 경우
+        solve(idx + 1, currentCnt);
+    }
+
+    static boolean canSelect(int p1, int p2) {
+        for (int pos = p1; pos <= p2; pos++) {
+            if (coordinate[pos]) {
+                return false;
             }
         }
-        System.out.println(cnt);
+        return true;
+    }
+
+    static void select(int p1, int p2) {
+        for (int pos = p1; pos <= p2; pos++) {
+            coordinate[pos] = true;
+        }
+    }
+    
+    static void unselect(int p1, int p2) {
+        for (int pos = p1; pos <= p2; pos++) {
+            coordinate[pos] = false;
+        }
     }
 }
