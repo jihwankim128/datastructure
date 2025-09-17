5bf33b254137fbaeae6d8accdf0514c344b48e30
Laboon <jihwan1535@nate.com>
2025-09-17 23:33:08 +0900

    Title: XOR 결과 최대 만들기, Time: 126ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250917/XOR 결과 최대 만들기/max-of-xor.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+
+    static int n, m, ans;
+    static int[] A;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        A = new int[n];
+        for (int i = 0; i < n; i++) {
+            A[i] = sc.nextInt();
+        }
+        
+        solve(0, 0, 0);
+        System.out.println(ans);
+    }
+
+    static void solve(int k, int cnt, int sum) {
+        if (k >= n) return;
+        if (cnt == m) {
+            ans = Math.max(ans, sum);
+            return;
+        }
+
+        solve(k + 1, cnt + 1, sum ^ A[k]);
+        solve(k + 1, cnt, sum);
+    }
+}
