bf0073de4db373a534c8eacf90d637daa710215d
Laboon <jihwan1535@nate.com>
2025-09-21 00:40:07 +0900

    Title: 최대 동전 거슬러주기, Time: 129ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250921/최대 동전 거슬러주기/max-coin-change.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Main {
+
+    static int n, m;
+    static int[] coin, dp;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        coin = new int[n];
+        dp = new int[m + 1];
+        for (int i = 0; i < n; i++) {
+            coin[i] = sc.nextInt();
+        }
+        solve();
+    }
+
+    static void solve() {
+        for (int c: coin) {
+            for (int i = c; i <= m; i++) {
+                dp[i] = Math.max(dp[i], dp[i - c] + 1);
+            }
+        }
+        System.out.println(dp[m]);
+    }
+}
