7ddce553880894006b0805f6cf7b26e5e00aebdb
Laboon <jihwan1535@nate.com>
2025-09-21 00:30:38 +0900

    Title: 1, 2, 5 더하기, Time: 124ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250921/1, 2, 5 더하기/1-2-5-plus.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Main {
+    
+    static int n;
+    static int[] dp = new int[1001];
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        solve();
+    }
+
+    static void solve() {
+        dp[0] = 1;
+        for (int i = 1; i <= n; i++) {
+            dp[i] = dp[i - 1];
+            if (i - 2 < 0) continue;
+            dp[i] += dp[i - 2];
+            if (i - 5 < 0) continue;
+            dp[i] += dp[i - 5];
+        }
+        System.out.println(dp[n]);
+    }
+}
