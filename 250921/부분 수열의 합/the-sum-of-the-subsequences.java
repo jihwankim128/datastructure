df44ddebff277d8380ebead47c3246182d24d06f
Laboon <jihwan1535@nate.com>
2025-09-21 01:59:17 +0900

    Title: 부분 수열의 합, Time: 132ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 250921/부분 수열의 합/the-sum-of-the-subsequences.java
@@ -0,0 +1,33 @@
+// AM 01:51 ~ 01:59 -> 8m
+import java.util.Scanner;
+
+public class Main {
+
+    static int n, m;
+    static int[] arr;
+    static boolean[] dp;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        arr = new int[n];
+        dp = new boolean[m + 1];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        
+        solve();
+    }
+
+    static void solve() {
+        for (int num: arr) {
+            for (int j = m; j >= num; j--) {
+                if (!dp[j - num]) continue;
+                dp[j] = true;
+            }
+            dp[num] = true;
+        }
+        System.out.println(dp[m] ? "Yes" : "No");
+    }
+}
