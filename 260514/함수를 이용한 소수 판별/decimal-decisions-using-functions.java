8823612a29bafcad54fdb083e32c066de2084dd1
이프, If <jihwan1535@nate.com>
2026-05-14 12:52:48 +0900

    [Passed] 함수를 이용한 소수 판별 (124ms, 10MB)

---
+++ 260514/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int sum = 0;
+        for(int i=a; i<=b; i++){
+            if(isPrime(i)){
+                sum+=i;
+            }
+        }
+        System.out.println(sum);
+
+    }
+
+    public static boolean isPrime(int num){
+        if(num==1){
+            return false;
+        }
+
+        for(int i=2; i<num ; i++){
+            if(num%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
