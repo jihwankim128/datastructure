e624eb57a319ed65192b68ff07f0fa0b28c1623d
이프, If <jihwan1535@nate.com>
2026-05-17 13:34:58 +0900

    [Passed] Factorial (127ms, 10MB)

---
+++ 260517/Factorial/factorial.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        System.out.println(Fac(n));
+    }
+
+    public static int Fac(int n){
+        if(n==1){
+            return 1;
+        }
+
+        return n*Fac(n-1);
+    }
+}
