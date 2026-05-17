a756727e8b935b5b5d693d0eff2004077593e80a
이프, If <jihwan1535@nate.com>
2026-05-17 11:18:57 +0900

    [Passed] 1이 되는 순간까지 (124ms, 10MB)

---
+++ 260517/1이 되는 순간까지/until-the-moment-I-reach-one.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(rec(n));
+    }
+
+    public static int rec(int n){
+
+        if(n==1){
+            return 0;
+        }
+
+        if(n%2==0){
+            return rec(n/2) + 1;
+        }
+        else{
+            return rec(n/3) + 1;
+        }
+    }
+}
