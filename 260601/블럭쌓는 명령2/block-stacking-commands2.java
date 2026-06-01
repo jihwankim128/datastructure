2100b774d074094c12e2c8dc9eee3e60603b8687
이프, If <jihwan1535@nate.com>
2026-06-01 19:43:41 +0900

    [Passed] 블럭쌓는 명령2 (127ms, 10MB)

--- 260601/블럭쌓는 명령2/block-stacking-commands2.java
+++ 260601/블럭쌓는 명령2/block-stacking-commands2.java
@@ -2,12 +2,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         int n = sc.nextInt();
         int k = sc.nextInt();
+
         int[] arr = new int[n+1];
-        for(int i=0; i<=n; i++){
-            arr[i] = 0;
-        }
+
         for(int i=0; i<k; i++){
             int from = sc.nextInt();
             int to = sc.nextInt();
