57f93247e11883a09066dc3fcf0283e5d826823a
이프, If <jihwan1535@nate.com>
2026-06-13 23:52:25 +0900

    [Passed] 만나는 그 순간 (238ms, 22MB)

--- 260613/만나는 그 순간/the-moment-we-meet.java
+++ 260613/만나는 그 순간/the-moment-we-meet.java
@@ -6,35 +6,24 @@ public class Main {
         int n = sc.nextInt();
         int m = sc.nextInt();
 
-        int[] arrA = new int[2000001];
-        int[] arrB = new int[2000001];
-        int positionA = 1000000;
-        int positionB = 1000000;
+        int[] arrA = new int[1000001];
+        int[] arrB = new int[1000001];
 
-        int curTimeA=0;
-        int curTimeB=0;
+        int curTimeA=1;
+        int curTimeB=1;
 
         for(int i=0; i<n; i++){
             char direction = sc.next().charAt(0);
             int num = sc.nextInt();
 
-            if(direction=='R'){
-                for(int j=0; j<=num; j++){
-                    arrA[curTimeA]=positionA;
-                    positionA++;
-                    curTimeA++;
+            for(int j=0; j<num; j++){
+                if(direction=='R'){
+                    arrA[curTimeA] = arrA[curTimeA-1] + 1;
                 }
-                curTimeA--;
-                positionA--;
-            }
-            else{
-                for(int j=0; j<=num; j++){
-                    arrA[curTimeA]=positionA;
-                    positionA--;
-                    curTimeA++;
+                else{
+                    arrA[curTimeA] = arrA[curTimeA-1] - 1;
                 }
-                curTimeA--;
-                positionA++;
+                curTimeA++;
             }
             
         }
@@ -42,29 +31,18 @@ public class Main {
             char direction = sc.next().charAt(0);
             int num = sc.nextInt();
 
-            if(direction=='R'){
-                for(int j=0; j<=num; j++){
-                    arrB[curTimeB]=positionB;
-                    positionB++;
-                    curTimeB++;
+            for(int j=0; j<num; j++){
+                if(direction=='R'){
+                    arrB[curTimeB] = arrB[curTimeB-1]+1;
                 }
-                curTimeB--;
-                positionB--;
-            }
-            else{
-                for(int j=0; j<=num; j++){
-                    arrB[curTimeB]=positionB;
-                    positionB--;
-                    curTimeB++;
+                else{
+                    arrB[curTimeB] = arrB[curTimeB-1]-1;
                 }
-                curTimeB--;
-                positionB++;
+                curTimeB++;
             }
         }
 
         int sameTime = 0;
-
-
         for(int i=1; i<=1000000; i++){
 
             if(arrA[i]==arrB[i] && arrA[i]!=0){
