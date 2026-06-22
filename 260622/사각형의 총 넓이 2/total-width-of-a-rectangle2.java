0bf4fcd49484dd08ac19ad7688c5319fc7892e56
이프, If <jihwan1535@nate.com>
2026-06-22 17:02:37 +0900

    [Passed] 사각형의 총 넓이 2 (136ms, 10MB)

---
+++ 260622/사각형의 총 넓이 2/total-width-of-a-rectangle2.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] arr = new int[200][200];
+
+        int n = sc.nextInt();
+        int offset = 100;
+        for(int i=0; i<n; i++){
+            int x1 = sc.nextInt() + offset;
+            int y1 = sc.nextInt() + offset;
+            int x2 = sc.nextInt() + offset;
+            int y2 = sc.nextInt() + offset;
+            
+            for(int j=x1; j<x2; j++){
+                for(int k=y1; k<y2; k++){
+                    if(arr[j][k]!=1){
+                        arr[j][k]+=1;
+                    }
+                }
+            }
+        }
+
+        int res = 0;
+        for(int i=0;i<200;i++){
+            for(int j=0;j<200;j++){
+                if(arr[i][j]==1){
+                    res+=1;
+                }
+            }
+        }
+        System.out.println(res);
+    }
+}
