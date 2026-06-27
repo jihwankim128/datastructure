031dac315023a8018ba6d0e93445c24d3227e4bb
이프, If <jihwan1535@nate.com>
2026-06-27 21:35:33 +0900

    [Passed] 연속되는 수 2 (196ms, 20MB)

--- 260627/연속되는 수 2/continuous-number2.java
+++ 260627/연속되는 수 2/continuous-number2.java
@@ -10,25 +10,18 @@ public class Main {
             arr[i] = sc.nextInt();
         }
 
-        int count=1;
-        int maxCount=0;
+        int cnt=0;
+        int ans=0;
         for(int i=0; i<n; i++){
-            if(i==0){
-                count=1;
-            }
-            else if(arr[i]==arr[i-1]){
-                count++;
+            if(i>=1 && arr[i]==arr[i-1]){
+                cnt++;
             }
             else{
-                if(maxCount<count){
-                    maxCount=count;
-                }
-                count=1;
+                cnt=1;
             }
+            ans = Math.max(cnt,ans);
         }
-        if(maxCount<count){
-            maxCount=count;
-        }
-        System.out.println(maxCount);
+
+        System.out.println(ans);
     }
 }
