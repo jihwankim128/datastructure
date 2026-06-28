0101b0616c6fb10615b6daa232fdf99fbc718c06
이프, If <jihwan1535@nate.com>
2026-06-28 18:20:01 +0900

    [Passed] 선두를 지켜라 (223ms, 21MB)

--- 260628/선두를 지켜라/keep-the-lead.java
+++ 260628/선두를 지켜라/keep-the-lead.java
@@ -33,31 +33,23 @@ public class Main {
             }
         }
 
-
-        int ans = 0;
-        int previousLeader = 0;
-
-        for (int i = 1; i < timeA; i++) {
-            int currentLeader = 0;
-
-            if (arrA[i] > arrB[i]) {
-                currentLeader = 1;
-            } else if (arrA[i] < arrB[i]) {
-                currentLeader = -1;
+        int leader = 0, ans = 0;
+        for(int i=1; i<timeA; i++){
+            if(arrA[i]>arrB[i]){
+                if(leader==2){
+                    ans++;
+                }
+                leader=1;
             }
-
-            // 공동 선두면 마지막 단독 선두 정보를 유지
-            if (currentLeader == 0) {
-                continue;
+            else if(arrB[i]>arrA[i]){
+                if(leader==1){
+                    ans++;
+                }
+                leader=2;
             }
+        }
 
-            // 처음 단독 선두가 생긴 것은 변경이 아님
-            if (previousLeader != 0 && previousLeader != currentLeader) {
-                ans++;
-            }
-
-            previousLeader = currentLeader;
-            }
+        
         System.out.println(ans);
         
     }
